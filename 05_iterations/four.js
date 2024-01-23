7626cef18862ac5f464c3e88d57e3308d505233e
vivekPatil45 <vivekrp4503@gmail.com>
2024-01-23 14:20:30 +0530

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,33 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //key give index
+    //console.log(programming[key]);
+}
+
+
+
+//not give error not print anything not iterable
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+// for of for Array
+// for in for Object
